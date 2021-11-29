662378a4c57737c5041b6281aec23b495a10633f
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-11-29 14:17:47 +0900

    Settings

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator,CardStyleInterpolators } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/LoginScreen/components/main';
+import SignUpScreen from './src/LoginScreen/SignUpScreen/components/main';
+
+const Stack=createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={
+          {
+            headerStyle:{backgroundColor:'#467FD3'},
+            headerTitleStyle:{color:'#ffffff'},
+            headerTitle: 'Memo App',
+            headerTintColor:'#ffffff',
+            headerBackTitle:'Back',
+            cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS,
+            gestureEnabled:true,
+            gestureDirection:'horizontal',
+          }
+        }
+        >
+          <Stack.Screen name="LogIn" component={LoginScreen} options={{
+            cardStyleInterpolator:CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}/>
+          <Stack.Screen name="SignUp" component={SignUpScreen} options={{
+            cardStyleInterpolator:CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}
+          />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/LoginScreen/components/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Text,TouchableOpacity} from 'react-native';
+
+const LoginScreen:React.FC = () => {
+    return(
+        <View>
+            <Text>testLogin</Text>
+            <TouchableOpacity >
+                <Text>MoveSignUp</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default LoginScreen;
